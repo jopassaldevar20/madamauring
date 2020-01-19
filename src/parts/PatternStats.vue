21d7b5b9afa53cc9c09659e905e30ac42820cc5b
jep <jopassaldevar20@gmail.com>
2020-01-19 22:58:23 +0800

    remove mapMutations

--- src/parts/PatternStats.vue
+++ src/parts/PatternStats.vue
@@ -8,10 +8,10 @@
 
                 <i class="material-icons">trending_up</i>
             </div>
-            
+
             <div class="ud__down">
                 <p>{{ addZeros(highestUpDown.down) }}</p>
-                
+
                 <i class="material-icons">trending_down</i>
             </div>
         </div>
@@ -19,15 +19,15 @@
         <div class="ps__equal_graph">
             <div class="eg__total_pattern">
                 <p class="tp__label">PATTERNS</p>
-                
+
                 <i class="material-icons">double_arrow</i>
-                
+
                 <p>{{ totalPattern }}</p>
             </div>
 
             <div class="eg__color_value">
                 <div
-                    v-for="(value, index) in equalUpDown" 
+                    v-for="(value, index) in equalUpDown"
                     :key="`label-${index}`"
                     class="cv__item"
                 >
@@ -40,7 +40,7 @@
                     <p>({{ value[1] }})</p>
                 </div>
             </div>
-            
+
             <div class="eg__total">
                 <p class="t__label">TOTAL</p>
 
@@ -51,7 +51,7 @@
 
             <div class="eg__graph">
                 <div
-                    v-for="(value, index) in equalUpDown" 
+                    v-for="(value, index) in equalUpDown"
                     :key="`graph-${index}`"
                     class="g__item"
                     :style="{ height: `${value[1]}px` }"
@@ -60,7 +60,7 @@
                 </div>
             </div>
         </div>
-            
+
         <div class="base_button blue" @click="getAllPattern">
             <p>UPDATE</p>
         </div>
@@ -68,7 +68,7 @@
 </template>
 
 <script>
-import { mapState, mapActions, mapMutations } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 
 import BaseCard from '@/components/BaseCard';
 
@@ -168,7 +168,7 @@ export default {
                 font-weight: 700;
                 line-height: 12px;
             }
-            
+
             > i {
                 margin: 0 4px;
                 font-size: 16px;
@@ -189,7 +189,7 @@ export default {
                     height: 10px;
                     margin-right: 4px;
                     position: relative;
-                    
+
                     > span {
                         width: 100%;
                         height: 100%;
@@ -205,7 +205,7 @@ export default {
                 > p {
                     font-size: 14px;
 
-                    &:last-child {  
+                    &:last-child {
                         margin-left: 4px;
                         font-size: 12px;
                     }
